91664dc6684a0ad62ce064f22f36a7678b3c8a4b
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-04-10 19:17:45 +0000

    Use package:prada environment rather than global - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@8158 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getPradaPar.R
+++ R/getPradaPar.R
@@ -1,15 +1,16 @@
 getPradaPar <- function(parname) {
   stopifnot(is.character(parname), length(parname)==1)
-  if(!exists("pradaPars", envir=globalenv()))
-    stop("Global options 'pradaPars' not defined in the global environment")
-  w <- which(parname==names(get("pradaPars", envir=globalenv())))
+  penv <- as.environment("package:prada")
+  if(!exists("pradaPars", envir=penv))
+    stop("Global options 'pradaPars' are not defined.")
+  w <- which(parname==names(get("pradaPars", envir=penv)))
   if (length(w)!=1)
      stop(paste("The parameter", parname, "is not defined"))
-  get("pradaPars", envir=globalenv())[[w]]
+  return(get("pradaPars", envir=penv)[[w]])
 }
 
 setPradaPars <- function(pars) {
   stopifnot(is.list(pars), is.character(names(pars)))
-  assign("pradaPars", pars, envir=globalenv())
+  assign("pradaPars", pars, envir=as.environment("package:prada"))
   invisible(NULL)
 }
