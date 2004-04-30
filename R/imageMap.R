f648052499df95a085167ef6394e36737d622c0c
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-04-30 21:21:41 +0000

    Added documentation and simplified plotPlate() - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@8456 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/imageMap.R
@@ -0,0 +1,17 @@
+imageMap = function(con, imgname, coord, tooltips, url, target="extra") {
+  stopifnot(is.character(imgname) && length(imgname)==1)
+  stopifnot(is.matrix(coord) && ncol(coord)==4)
+  n <- nrow(coord)
+  stopifnot(is.character(tooltips) && length(tooltips)==n)
+  stopifnot(is.character(url) && length(url)==n)
+  stopifnot(is.character(target), !any(is.na(target)))
+  stopifnot(!any(is.na(coord)), !any(is.na(tooltips)), !any(is.na(url)))
+  
+  mapname <- paste("map", gsub(" |/|#", "_", imgname), sep="_")
+  base::writeLines(paste("<IMG SRC=\"", imgname, "\" USEMAP=\#", mapname, " BORDER=0>", 
+                   "<MAP NAME=\"", mapname, "\">", sep=""), con)
+  base::writeLines(paste("<AREA SHAPE=\"rect\" HREF=\"", url, "\" TITLE=\"", tooltips,
+                   "\" COORDS=\"", coord[,1], ",", coord[,2], ",", coord[,3], ",",
+                   coord[,4], "\" TARGET=", target, "\">", sep=""), con)
+}
+
--- R/plotPlate.R
+++ R/plotPlate.R
@@ -1,33 +1,37 @@
-## plot a statistic
-## x must be a dataframe with columns "well" and "stat"
-plotPlate  = function(x, stat, main, zlim=NULL, col, device, plotfile=NULL, width,
-                      na.action="zero") {
-  nrwell = 96 
-  sizex  = 12
-  sizey  = 8
-  js = which(colnames(x)==stat)
-  if(length(js)!=1)
-    stop(paste("'x' must contain exactly one column with name '", stat, "'\n", sep=""))
-  jw = which(colnames(x)=="well")
-  if(length(jw)!=1)
-    stop("'x' must contain exactly one column with name 'well'\n")
-  if(any((x[,jw]<1) | (x[,jw]>nrwell)))
-    stop(paste("'x$well' must be between 1 and", nrwell, "\n"))
-  if(any(duplicated(x[,jw]) | is.na(x[,jw])))
-    stop("'x$well' must not contain NA or duplicates.\n")
-  if(is.null(zlim))
-    zlim=range(x[,js], na.rm=TRUE)
-  if(!is.numeric(zlim) || length(zlim)!=2 || any(is.na(zlim)))
-    stop("'zlim' mist be a numeric vector of length 2 with no NA.")
+plotPlate  = function(x, nrow=8, ncol=12, main, xrange, col,
+  device, file, width, na.action="zero") {
+
+  if(!is.numeric(ncol)||length(ncol)!=1)
+    stop("'ncol' must be a numeric vector of length 1")	
+  if(!is.numeric(nrow)||length(nrow)!=1)
+    stop("'nrow' must be a numeric vector of length 1")	
+  nrwell <- ncol*nrow
+  if(!is.numeric(x) || !is.vector(x) || length(x)!=nrwell)
+    stop("'x' must be a numeric vector of length 'ncol*nrow'")
+  if(missing(xrange))
+    xrange=range(x, na.rm=TRUE)
+  if(!is.numeric(xrange) || length(xrange)!=2 || any(is.na(xrange)))
+    stop("'xrange' must be a numeric vector of length 2 with no NA.")
 
   ## user coordinates: x=(-0.5...13.5), y=(-0.5...9.5)
-  xlim   = c(-0.5, sizex+1.5)
-  ylim   = c(-0.5, sizey+1.5)
+  xlim   = c(-0.5, ncol+1.5)
+  ylim   = c(-0.5, nrow+1.5)
   colbarwid = 0.3
   fw     = diff(xlim)+colbarwid
   fh     = diff(ylim)
-  height = width/fw * fh
-  do.call(device, args=list(file=plotfile, width=width, height=height))
+
+  if(missing(width))
+    stop("Argument 'width' is missing, with no default")
+  height <- width/fw * fh
+  args <- list(width=width, height=height)
+
+  if(!missing(device)) {
+    if(!is.character(device))
+      stop("'device' must be a character")
+    if(!missing(file))
+      args <- append(args, file)
+    do.call(device, args=args)
+  } 
   layout(matrix(1:2, ncol=2), widths=c(diff(xlim), colbarwid), heights=1)
 
   ## device coordinates
@@ -37,38 +41,38 @@ plotPlate  = function(x, stat, main, zlim=NULL, col, device, plotfile=NULL, widt
   par(mai=c(0,0,0,0))
   cex = 1.5
   plot(x=0, y=0, type="n", bty="n", xaxt="n", yaxt="n", xaxs="i", yaxs="i", xlim=xlim, ylim=ylim)
-  text((1:sizex), 0, paste(1:sizex), adj=c(0.5,0), cex=cex)
-  text(0, (sizey:1), c("A","B","C","D","E","F","G","H"), adj=c(0, 0.5), cex=cex)
+  graphics::text((1:ncol), 0, paste(1:ncol), adj=c(0.5,0), cex=cex)
+  graphics::text(0, (nrow:1), c("A","B","C","D","E","F","G","H"), adj=c(0, 0.5), cex=cex)
   if(!missing(main))
-    text((sizex+1)/2, sizey+1, main, adj=c(0.5, 1), cex=cex)
+    graphics::text((ncol+1)/2, nrow+1, main, adj=c(0.5, 1), cex=cex)
   
   nrcolors   = 256
   thepalette = colorramp(col)(nrcolors)
 
   # the mapping from values to color indices
-  z2icol = function(z) {
-    res = round((z-zlim[1])/diff(zlim)*(nrcolors-1))+1
+  z2icol <- function(z) {
+    res = round((z-xrange[1])/diff(xrange)*(nrcolors-1))+1
     res[res > nrcolors] = nrcolors
     res[res < 1       ] = 1
     return(res)
   }
-  icol2z = function(i) {
-    (i-1)/(nrcolors-1)*diff(zlim)+zlim[1]
+  icol2z <- function(i) {
+    (i-1)/(nrcolors-1)*diff(xrange)+xrange[1]
   }
   stopifnot(all(z2icol(icol2z(1:nrcolors))==1:nrcolors))
-  circcol = thepalette[z2icol(x[,js])]
+  circcol <- thepalette[z2icol(x)]
 
   ## circles
   radius = 0.45
   xc = radius*cos(seq(0, 2*pi, len=73))
   yc = radius*sin(seq(0, 2*pi, len=73))
-  x0 = 1     + ((x[,jw]-1)  %% sizex)
-  y0 = sizey - ((x[,jw]-1) %/% sizex)
+  x0 = 1     + (0:(nrwell-1)) %% ncol
+  y0 = nrow - (0:(nrwell-1)) %/% ncol
 
   switch(na.action,
          zero = {
            circcol[is.na(circcol)] <- thepalette[z2icol(0)]
-           wh <- 1:nrow(x)
+           wh <- 1:nrwell
          }, 
          omit = {
            wh <- which(!is.na(circcol))
@@ -83,28 +87,26 @@ plotPlate  = function(x, stat, main, zlim=NULL, col, device, plotfile=NULL, widt
   }
   
   xmin = 0.5
-  xmax = sizex + 0.5
+  xmax = ncol + 0.5
   ymin = 0.5
-  ymax = sizey + 0.5
+  ymax = nrow + 0.5
   polygon(c(xmin, xmax, xmax, xmin, xmin), c(ymin, ymin, ymax, ymax, ymin))
         
   par(mai=0.5*c(1,0,1,0))
   image(x=0, y=icol2z(1:nrcolors), z=matrix(1:nrcolors, nrow=1),
         col = thepalette, xaxt="n")
 
-  if(device %in% c("pdf", "png", "jpeg"))
-    dev.off()
+  if(!missing(device) && device %in% c("pdf", "png", "jpeg"))
+    dev.off()	   
 
-  x0 = 1 + (wh-1) %%  sizex
-  y0 = 1 + (wh-1) %/% sizex
+  x0 = 1 + (wh-1) %%  ncol
+  y0 = 1 + (wh-1) %/% ncol
   dx = dy = 0.4
   x1 = u2px(x0-dx)
   x2 = u2px(x0+dx)
   y1 = u2py(y0-dy)
   y2 = u2py(y0+dy)
 
-  res = list(which=wh, coord=floor(cbind(x1, y1, x2, y2) + 0.5))
-  
-  class(res) = "plotPlateResult"
-  return(res)
+  res <- list(which=wh, coord=floor(cbind(x1, y1, x2, y2) + 0.5))
+  invisible(res)
 }
