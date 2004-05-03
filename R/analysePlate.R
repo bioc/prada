91cbe323a239e91b4e4ac2aeb3771d2c2780da02
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-05-03 19:16:27 +0000

    Updates - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@8473 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/analysePlate.R
+++ R/analysePlate.R
@@ -1,5 +1,5 @@
 ## apply a statistic to the data from each well in a plate
-analysePlate <-  function(x, wellcol="well", statfun, platename, plotdir=".", ...) {
+analysePlate <-  function(x, wellcol="well", wellrange, statfun, platename, plotdir=".", ...) {
 
   jw <- which(colnames(dat)==wellcol)
   if(length(jw)!=1)
@@ -8,9 +8,10 @@ analysePlate <-  function(x, wellcol="well", statfun, platename, plotdir=".", ..
 
   stopifnot(is.character(statfun), is.character(plotdir), is.character(platename))
   stopifnot(length(statfun)==1, length(plotdir)==1, length(platename)==1)
-
+  stopifnot(is.vector(wellrange), all(unique(x[, jw]) %in% wellrange))
+  
   y  <- NULL
-  for (w in sort(unique(x[, jw]))) {
+  for (w in wellrange) {
     rv <- do.call(statfun, list(x = x[x[, jw]==w, ],
                   plotdir  = outdir, ...))
     stopifnot(all(sapply(rv, length) == 1))
@@ -23,7 +24,7 @@ analysePlate <-  function(x, wellcol="well", statfun, platename, plotdir=".", ..
     ## to prevent as.data.frame.character being called and converting to factor:
     for (i in which(crv=="character"))
       class(rv[[i]]) <- "vector"
-    y  <- rbind(y, data.frame(platename, w, rv))
+    y  <- rbind(y, data.frame(I(platename), w, rv))
   }
   
   if(getPradaPar("debug"))
