03661696b76d821679071c8792ce4be00f583ecc
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-07-15 16:01:43 +0000

    bug fix and new class cytoSet - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@9417 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cytoFrame.R
+++ R/cytoFrame.R
@@ -1,36 +1,46 @@
+library(Biobase)
+
 setClass("cytoFrame",
-         representation(exprs="matrix",
-                        description="character"))
+  representation(exprs="matrix",
+                 description="character"),
+  prototype=list(exprs=matrix(numeric(0), nrow=0, ncol=0),
+                 description=c(note="empty")),
+ validity=function(object){
+   is.matrix(object@exprs)&&is.character(object@description)
+ })
+
+##These are already defined as generic functions in Biobase:
+##if(!isGeneric("exprs<-"))
+##  setGeneric("exprs<-", function(object, value)
+##    standardGeneric("exprs<-"))
+##
+##if(!isGeneric("exprs"))
+##  setGeneric("exprs", function(object)
+##    standardGeneric("exprs"))
+##
+##if(!isGeneric("description<-"))
+##  setGeneric("description<-", function(object, value)
+##    standardGeneric("description<-"))
+##
+##if(!isGeneric("description"))
+##  setGeneric("description", function(object)
+##    standardGeneric("description"))
 
 ## accessor methods for slots exprs and description
-if(!isGeneric("exprs<-"))
-  setGeneric("exprs<-", function(object, value)
-    standardGeneric("exprs<-"))
-
-if(!isGeneric("exprs"))
-  setGeneric("exprs", function(object)
-    standardGeneric("exprs"))
-
-if(!isGeneric("description<-"))
-  setGeneric("description<-", function(object, value)
-    standardGeneric("description<-"))
-
-if(!isGeneric("description"))
-  setGeneric("description", function(object)
-    standardGeneric("description"))
-   
 setMethod("exprs",      "cytoFrame", function(object) object@exprs)
 setMethod("description", "cytoFrame", function(object) object@description)
 
 setReplaceMethod("exprs", c("cytoFrame", "matrix"),
    function(object, value) {
      object@exprs <- value
-     object })
+     return(object)
+   })
 
 setReplaceMethod("description", c("cytoFrame", "character"),
    function(object, value) {
      object@description <- value
-     object })
+     return(object)
+   })
 
 "$.cytoFrame" <- function(x, val)
     (description(x))[val]
@@ -56,7 +66,79 @@ setReplaceMethod("[", "cytoFrame", function(x, i, j, ..., value) {
   x
   })
 
-## Biobase
-## if (!isClass("exprList"))
-##    setClassUnion("exprList", c("list", "environment"))
 
+setClass("cytoSet",
+  representation(frames="environment",
+                 phenoData="phenoData",
+                 colnames="character"),
+  prototype=list(frames=new.env(),
+                 phenoData=new("phenoData",
+                   pData=data.frame(framename=I(character(0))),
+                   varLabels=list(framename="Name in frame")),
+                 colnames=character(0)),
+  validity=function(object){
+    is(object@phenoData, "phenoData") &&
+    is(object@colnames, "character") &&
+    is(object@frame, "environment") &&
+    "framename" %in% colnames(pData(object@phenoData)) &&
+    setequal(ls(object@frame), object@phenoData$wellname) &&
+    all(sapply(ls(object@frame), function(x)
+      { is(get(x, inherits=FALSE), "cytoFrame") &&
+        colnames(exprs(get(x, inherits=FALSE)))==NULL })) &&
+    all(sapply(ls(object@frame), ncol)==length(object@colnames))
+  })
+
+setMethod("[", "cytoSet", function(x, i, j, ..., drop=FALSE) {
+  if(!missing(j))
+    stop("incorrect number of dimensions")
+  fr <-new.env(hash=TRUE)
+  nm <- object@phenoData$framename[i]
+  multiassign(nm, mget(nm, object@frames), envir=fr, inherits=FALSE) 
+  new("cytoSet",
+      frames=fr,
+      phenoData=x@phenoData[i, ],
+      colnames=x@phenoData)
+}, valueClass="cytoSet")
+
+setMethod("[[", "cytoSet", function(x, i, j, ...) {
+  if(!missing(j))
+    stop("incorrect number of dimensions")
+  if(length(i)!=1)
+    stop("subscript out of bounds (index must have length 1 in '[[')")
+  rv <- get(x@phenoData$framename[i], x@frames, inherits=FALSE)
+  colnames(exprs(rv)) <- x@colnames
+  return(rv)
+}, valueClass="cytoFrame")
+
+## show method for cytoSet
+setMethod("show", "cytoSet", function(object) {
+  cat("cytoSet object which contains", nrow(object@phenoData), "cytoFrames ",
+      "with colnames\n", colnames(object), "\n")
+  show(phenoData(object))
+})
+
+## get and set colnames slot of cytoSet
+if(!isGeneric("setColnames<-"))
+  setGeneric("setColnames<-", function(object, value)
+    standardGeneric("setColnames<-"))
+
+if(!isGeneric("getColnames"))
+  setGeneric("getColnames", function(object)
+    standardGeneric("getColnames"))
+
+setMethod("getColnames", "cytoSet", function(object) object@colnames)
+setReplaceMethod("setColnames", c("cytoSet", "character"),
+  function(object, value) {
+    object@colnames <- value
+    return(object)
+  })      
+
+## get and set phenoData slot of cytoSet
+setMethod("phenoData", "cytoSet", function(object) object@phenoData)
+setReplaceMethod("phenoData", c("cytoSet", "phenoData"),
+  function(object, value) {
+    object@phenoData <- value
+    return(object)
+  })      
+
+          
