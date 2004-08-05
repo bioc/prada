991379709ebf6367039608b0af8b402d034036e6
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-08-05 15:54:17 +0000

    Updates - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@9621 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/smoothscatter.R
+++ R/smoothscatter.R
@@ -1,6 +1,6 @@
 smoothScatter <- function(x, y=NA, nrpoints=100,
                            colrange=c("white", "#231d1d"),
-                           nbin=200, bandwidth=20, ...){
+                           nbin=200, bandwidth, ...){
   
     # check correct input
     if (!is.matrix(x) || ncol(x)<2)
@@ -10,15 +10,21 @@ smoothScatter <- function(x, y=NA, nrpoints=100,
     nrpoints <- floor(nrpoints)
     if (!is.character(colrange))
       stop("colrange must be character vector containing valid color identifiers")
-    if (!is.numeric(nbin) | !is.numeric(bandwidth))
-      stop("'nbin' and 'bandwidth' must be numeric")
-    if ( length(nbin) == 1 )
-        nbin <- c(nbin, nbin)
+    if (!is.numeric(nbin) || (!length(nbin) %in% 1:2))
+      stop("'nbin' must be numeric of length 1 or 2")
+    if (length(nbin) == 1)
+      nbin <- c(nbin, nbin)
 
-    # create density map
+    if (missing(bandwidth)) {
+      bandwidth <- diff(apply(x, 2, quantile, probs=c(0.05, 0.95))) / 10
+      } else {
+      if(!is.numeric(bandwidth))
+        stop("'nbin' and 'bandwidth' must be numeric")
+     }
+                                        # create density map
     map  <- bkde2D(x, gridsize=nbin, bandwidth=bandwidth)
-    xm    <- map$x1
-    ym    <- map$x2
+    xm   <- map$x1
+    ym   <- map$x2
     dens <- map$fhat
     m    <- nrow(x)
   
@@ -26,11 +32,17 @@ smoothScatter <- function(x, y=NA, nrpoints=100,
     cols <- colorramp(colrange) (256)
     image(xm, ym, z=dens, col=cols, ...)
     box()
-    
+
     # plot selection of dots
     if (nrpoints!=0){
+      ## we assume that map$x1 and map$x2 go linearly from
+      ## their first to their last value in nbin steps
+      stopifnot(length(xm)==nbin[1], length(ym)==nbin[2], all(dim(dens)==nbin))
+      ixm <- round((x[,1]-xm[1])/(xm[nbin[1]]-xm[1])*(nbin[1]-1))
+      iym <- round((x[,2]-ym[1])/(ym[nbin[2]]-ym[1])*(nbin[2]-1))
+      idens <- dens[1+iym*nbin[1]+ixm]
       nrpoints <- min(m, nrpoints)
-      sel <- sample(1:m, size=nrpoints)
+      sel <- order(idens, decreasing=FALSE)[1:nrpoints]
       points(x[sel,1:2], pch=".", col="black")
     }
 }
