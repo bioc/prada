969ae858e40419956fea7c9a185bbc5f178129d1
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-10-22 14:19:56 +0000

    bugfix! these files were missing - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@10211 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/combineFrames.R
@@ -0,0 +1,28 @@
+combineFrames <- function(x, by) {
+  if(!is(x, "cytoSet"))
+    stop("'x' must be a cytoSet.")
+  if(!is.factor(by))
+    by<-factor(by)
+  if(length(by)!=length(x))
+    stop("Length of 'by' must be the same as length of 'x'.")
+  e  <- new.env(hash=TRUE)
+  df <- data.frame(name=I(levels(by)))
+  vl <- list(name="Grouping factor, obtained from argument 'by' of 'combineFrames'")
+  for(i in 1:ncol(pData(x))) {
+    s <- split(pData(x)[[i]], by)
+    s <- lapply(s, unique)
+    if(all(listLen(s)==1)) {
+      df <- cbind(df, unlist(s))
+      vl <- append(vl, varLabels(phenoData(x))[[i]])
+      colnames(df)[ncol(df)] <- names(vl)[length(vl)] <- colnames(pData(x))[i]
+    }
+  }
+  for(lev in df$name) {
+    cf <- new("cytoFrame", exprs=do.call("rbind", args=csApply(x[which(by==lev)], function(z) z, simplify=FALSE)))
+    colnames(cf) <- NULL
+    assign(lev, cf, envir=e)
+  }
+  new("cytoSet", frames=e,
+      phenoData=new("phenoData", pData=df, varLabels=vl),
+      colnames=colnames(x))
+}
