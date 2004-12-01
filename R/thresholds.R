84623e7cf37f04f8d25afce389bc2bc7e3823f3c
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-12-01 16:21:00 +0000

    fixed threshold issue, bumped to version 1.1.13 FH -This line, and those below, will be ignored--
    A    R/thresholds.R
    A    man/thresholds.Rd
    M    DESCRIPTION
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@10615 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/thresholds.R
@@ -0,0 +1,15 @@
+thresholds = function(x, y, xthr, ythr){
+  xn    <- .pradaMakeX(x, y)
+  if(!is.numeric(xthr) | !is.numeric(ythr))
+    stop("'xthr' and 'ythr' must be numeric vectors")
+ 
+  bx  <- (xn[,1]<=xthr)
+  by  <- (xn[,2]<=ythr)
+  nbx <- !bx
+  nby <- !by
+  return(rbind(c(sum( bx&nby), sum(nbx&nby)),
+               c(sum( bx& by), sum(nbx& by))))
+} 
+
+
+ 
