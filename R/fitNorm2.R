6fe30e76b2eb8a40db07b6257e105f48920efc08
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2005-05-06 10:54:55 +0000

    FH: moved smoothScatter and densCols to package geneplotter
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@12216 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fitNorm2.R
+++ R/fitNorm2.R
@@ -1,6 +1,17 @@
+.pradaMakeX    <- function(x, y) {
+  ## check correct input
+  if (!is.matrix(x) || ncol(x)<2)
+    x <- cbind(x, y)
+  if (!is.matrix(x) && ncol(x)==2)
+    stop("Wrong size of arguments 'x' (and/or 'y')")
+  return(x)
+}
+
+
 fitNorm2 <- function(x, y=NA, scalefac=1, method="covMcd", noise) {
 
-  require(rrcov)
+  if(!require(rrcov))
+    stop("Required package rrcov could not be found.")
   #require(MASS)
 
   if (!(is.matrix(x) && ncol(x)==2)){
