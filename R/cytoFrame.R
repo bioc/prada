97f12019da0e40335aa27c6efb7bb3c9886c87ef
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2005-10-26 15:52:27 +0000

    missing generics are now imported from the Biobase namespace
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@14592 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cytoFrame.R
+++ R/cytoFrame.R
@@ -42,7 +42,7 @@ setClass("cytoSet",
 
 
 ############################### Generic functions ##########################
-##These are already defined as generic functions in Biobase:
+##These are already defined as generic functions:
 
 if(!isGeneric("colnames<-"))
   setGeneric("colnames<-",    function(x, value) standardGeneric("colnames<-"))
@@ -59,6 +59,59 @@ if(!isGeneric("plot"))
 
 
 ####################### formal methods ###############################
+
+
+## accessor method for slot exprs
+setMethod("exprs",
+  signature="cytoFrame",
+  definition=function(object) object@exprs,
+  valueClass="matrix")
+
+## accessor method for slot description
+setMethod("description",
+  signature="cytoFrame",
+  definition=function(object) object@description,
+  valueClass="character")
+
+## accessor method for slot phenoData
+setMethod("phenoData",
+  signature="cytoSet",
+  definition=function(object) object@phenoData,
+  valueClass="phenoData")
+
+##  accessor method for slot phenoData@pData
+setMethod("pData",
+  signature="cytoSet",
+  definition=function(object) object@phenoData@pData,
+  valueClass="data.frame")
+
+## replace method for slot exprs
+setReplaceMethod("exprs",
+   signature=c("cytoFrame", "matrix"),
+   definition=function(object, value) {
+     object@exprs <- value
+     return(object)
+   })
+
+## replace method for slot description
+setReplaceMethod("description",
+   signature=c("cytoFrame", "character"),
+   definition=function(object, value) {
+     object@description <- value
+     return(object)
+   })
+  
+## replace method for slot phenoData
+setReplaceMethod("phenoData", c("cytoSet", "phenoData"),
+  function(object, value) {
+    object@phenoData <- value
+    return(object)
+  })      
+
+
+
+
+
 
 ## accessor method for slot colnames cytoFrame
 setMethod("colnames",
