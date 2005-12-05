3b16aae932143872a9c87b77dc428a0f8f6a7be5
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2005-12-05 17:45:45 +0000

    fixed bug in plotPlateGrid
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@15188 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotPlateGrid.R
+++ R/plotPlateGrid.R
@@ -233,7 +233,7 @@ plotPlateGrid <- function (x, gridCall="circle", callArgs=NULL, nrow = 8, ncol =
     pushViewport(vp9)
     grid.text(main, gp=gpar(fontsize=fontsize, cex=1.8, fontface="bold"))
   }
-  popViewport(1)
+  popViewport(2)
   
   ## return value ##
   res <- list(which=wh)
