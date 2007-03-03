a78365ee264d7fa90c9678dd29abb8007db0eec7
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2007-03-03 11:03:13 +0000

    updated progress bar function to give more meaningful textual reports
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/prada@22935 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tcltkProgress.R
+++ R/tcltkProgress.R
@@ -1,44 +1,70 @@
+progress <- function(title="processing task...", message="",
+                     sub=""){
 
-progress <- function(title="processing task...", message=""){
+  ## check arguments
   options(warn=-1)
   require(tcltk)
-  if(!is.character(title) || !is.character(message))
+  if(!is.character(title) || !is.character(message)|| !is.character(sub))
     stop("All arguments must be character vectors of length 1")
+
+  ## set up necessary variables in global environment 
   assign(".tkprogress.canceled", FALSE, .GlobalEnv)
   assign(".tkprogress.window", tktoplevel(), .GlobalEnv)
-  tkwm.geometry(.tkprogress.window, "250x140")
+  assign(".tkprogress.iterator", tclVar("0"), .GlobalEnv)
+  assign(".tkprogress.fallbackIterator", tclVar("0 %"), .GlobalEnv)
+  assign(".tkprogress.labelText", tclVar("0 %"), .GlobalEnv)
+
+  ## the tcl/tk toplevel window
+  tkwm.geometry(.tkprogress.window, "280x140")
   tkwm.title(.tkprogress.window, title)
   tkconfigure(.tkprogress.window, cursor="watch")
-  assign(".tkprogress.labelText", tclVar("0"), .GlobalEnv)
   frame <- tkframe(.tkprogress.window, relief="groove",borderwidth=2)
-  font <- tkfont.create(family="sansserif",size=14,weight="bold")
-  font2 <- tkfont.create(family="sansserif",size=12,weight="bold")
-  label <- tklabel(frame, text=tclvalue(.tkprogress.labelText), font=font)
+  font <- tkfont.create(family="helvetica",size=13,weight="bold")
+  font2 <- tkfont.create(family="helvetica",size=11,weight="bold")
+  font3 <- tkfont.create(family="helvetica",size=11)
+
+  ## the textual status indicator
+  if(sub!="")
+    tclvalue(.tkprogress.labelText) <<- sub
+  label <- tklabel(frame, text=tclvalue(.tkprogress.labelText), font=font3)
   tkconfigure(label,textvariable=.tkprogress.labelText)
-  tkgrid(tklabel(.tkprogress.window, text=""))
+
+  ## the header message
   if(message!=""){
     tkgrid(tklabel(.tkprogress.window, text=message, font=font2))
     tkgrid(tklabel(.tkprogress.window, text=""))
   }
+
+  ## use BWidgetsprogress bar widget or fallback to textual indicator
   if(as.character(tclRequire("BWidget"))!="FALSE"){
-    ## tcltk progress bar from BWidgets library##
     progBar <- tkwidget(.tkprogress.window, "ProgressBar",
-                        variable=.tkprogress.labelText)
+                        variable=.tkprogress.iterator)
     tkgrid(progBar)
-    tkgrid(tklabel(.tkprogress.window, text=""))
     tkconfigure(frame, borderwidth=0)
+    tkgrid(label)
+  }else{
+    itLabel <- tklabel(frame, text=tclvalue(.tkprogress.fallbackIterator), 
+                       font=font)
+    tkconfigure(itLabel,textvariable=.tkprogress.fallbackIterator)
+    tkgrid(itLabel)
+    if(sub!="")
+      tkgrid(label)
   }
-  tkgrid(label, tklabel(frame, text="% done", font=font))
   tkgrid(frame)
-  tkgrid(tklabel(.tkprogress.window, text=""))
   tkbind(.tkprogress.window, "<Destroy>", function(){
     .tkprogress.canceled <<- TRUE; killProgress()})
   options(warn=0)
 }
 
-updateProgress <- function(percentage, autoKill=FALSE){
+updateProgress <- function(percentage, autoKill=FALSE, sub=""){
   percentage <- as.integer(percentage)
-  tclvalue(.tkprogress.labelText) <<- percentage
+  tclvalue(.tkprogress.iterator) <<- percentage
+  tclvalue(.tkprogress.fallbackIterator) <<- paste(percentage, "%")
+  if(sub==""){
+    tclvalue(.tkprogress.labelText) <<- paste(percentage, "%")
+  }else{
+    tclvalue(.tkprogress.labelText) <<- sub
+  }
   tcl("update", "idletasks")
   if(autoKill)
     if(percentage>=100)
@@ -51,4 +77,6 @@ killProgress <- function(){
 }
 
 
-#for(i in 1:50) {rnorm(1e+05); updateProgress(i*2, auto=T)}
+#progress(message="reading in multiple files...", sub="(1 of 50 files)")
+#for(i in 1:50) {rnorm(1e+05); updateProgress(i*2, autoKill=TRUE, 
+#                                  sub=paste("(", i, " of 50 files)", sep=""))}
